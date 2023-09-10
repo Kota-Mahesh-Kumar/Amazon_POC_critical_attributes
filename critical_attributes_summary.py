84b515d1890c2779f718f49f14a1015066eb3f79
Kota-Mahesh-Kumar <72548938+Kota-Mahesh-Kumar@users.noreply.github.com>
2023-09-10 23:40:22 +0530

    Update critical_attributes_summary.py

--- critical_attributes_summary.py
+++ critical_attributes_summary.py
@@ -13,6 +13,16 @@ import matplotlib.pyplot as plt
 # Read the data
 excel_path =r"Amazon_Dimension_analysis_Dashboard_v1.xlsx"
 
+
+st.set_page_config(layout="wide")
+
+st.markdown(
+    """
+    <h1 style="text-align:center;">Critical Attributes Summary</h1>
+    """,
+    unsafe_allow_html=True
+)
+
 def data_for_streamlit(excel_path):
     data = pd.read_excel(excel_path, sheet_name = 0)
     data.drop("Unnamed: 0", axis = 1, inplace = True)
@@ -52,23 +62,27 @@ def rename_the_columns(df):
 df_original, columns = data_for_streamlit(excel_path)
 asin_list = df_original['ASIN'].tolist() 
 
-critical_attributes_list = df_original.columns[9:16].tolist()
+critical_attributes_list = df_original.columns[9:18].tolist()
 
-col1, col2 = st.columns(2)
-with col1:
-    asin = st.selectbox("ASIN",asin_list, key = "asin_number")
-with col2:
-    critical_attribute = st.selectbox("Critical Attribute",critical_attributes_list, key = "critical_attribute")
+# col1 = st.columns(1)
+# with col1:
+asin = st.selectbox("ASIN",asin_list, key = "asin_number")
+# with col2:
+#     critical_attribute = st.selectbox("Critical Attribute",critical_attributes_list, key = "critical_attribute")
 
 
 if 'asin_number' not in st.session_state:
     st.session_state['asin_number'] = asin_list[0]
 
-if "critical_attribute" not in st.session_state:
-    st.session_state['critical_attribute'] = critical_attributes_list[0]
+# if "critical_attribute" not in st.session_state:
+#     st.session_state['critical_attribute'] = critical_attributes_list[0]
 
 asin_key = st.session_state['asin_number']
-critical_attributes_key = st.session_state['critical_attribute']
+# critical_attributes_key = st.session_state['critical_attribute']
+
+Final_column_list = ['Inspection Report', 'Amazon Technical Details', 'Title', 'Description', 'Bullet 1', 'Bullet 2', 'Bullet 3', 'Bullet 4', 'Bullet 5', 'Bullet 6', 'Bullet 7', 'Bullet 8', 
+                        'Bullet 9', 'A+ Text 1', 'A+ Text 2', 'A+ Text 3', 'A+ Text 4', 'A+ Text 5', 'A+ Text 6', 'A+ Text 7', 'A+ Text 8', 'A+P 1', 'A+P 2', 'A+P 3', 'A+P 4', 'A+P 5', 'A+P 6', 'A+P 7', 'A+P 8', 'A+P 9',
+                        'MI', 'AI 1', 'AI 2', 'AI 3', 'AI 4', 'AI 5', 'AI 6', 'AI 7', 'AI 8', 'AI 8-1', 'AI 8-2', 'AI 9', 'AI 10']
 
 def dataframe_after_filtering(df,asin_key,critical_attributes_key):
     asin_df_selected = df[df['ASIN'] == asin_key]
@@ -77,13 +91,35 @@ def dataframe_after_filtering(df,asin_key,critical_attributes_key):
     dataframe_for_attributes_asin = asin_df_selected[selected_columns]
     return dataframe_for_attributes_asin, len(final_output), selected_columns
 
-dataframe_for_attributes_asin, length, selected_columns = dataframe_after_filtering(df_original,asin_key,critical_attributes_key)
+dataframes_by_critical_attribute = {}
+for critical_attributes_key in critical_attributes_list:
+    dataframe_for_attributes_asin, final_output, selected_columns = dataframe_after_filtering(df_original,asin_key,critical_attributes_key)
+    try:
+        dataframe_for_attributes_asin.columns  = Final_column_list
+    except:
+        dataframe_for_attributes_asin.columns = ['Amazon Technical Details']
+    dataframe_for_attributes_asin_changed = pd.melt(dataframe_for_attributes_asin)
+    #melted_df = pd.melt(dataframe_for_attributes_asin, id_vars=['variable'], var_name='Column', value_name='Value')
+    dataframes_by_critical_attribute[critical_attributes_key] = dataframe_for_attributes_asin_changed['value']
+
 st.write('')
 st.write('')
-st.write('')
-st.dataframe(dataframe_for_attributes_asin, use_container_width = True)
+
+def highlight_cells(value):
+    if value is not None or 'cm' in str(value):
+        return 'color: red'
+    else:
+        return ''
 
 
+combined_dataframe = pd.DataFrame(dataframes_by_critical_attribute)
+combined_dataframe['Variable'] = Final_column_list
+columns_order = ['Variable', "Length", "Breadth", "Height" ,"Radius", "Item Weight", "Item Volume /Capacity", "Net Quantity","Material","Colour"]
+combined_dataframe = combined_dataframe[columns_order]
+styled_df = combined_dataframe.style.applymap(highlight_cells)
+
+st.dataframe(styled_df, use_container_width = True)
+    # st.dataframe(dataframe_for_attributes_asin_changed.style.applymap(lambda x: 'color: red' if any('cm' in words for words in x.split()) else ''))
 
 
 
